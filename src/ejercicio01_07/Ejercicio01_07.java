0dec7bc52d554cceab07f81f69a6d30ee8e250b2
user <user@192.168.1.2>
2017-09-14 02:43:15 -0500

    0107 commit

---
+++ src/ejercicio01_07/Ejercicio01_07.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio01_07;
+
+/**
+ *
+ * @author user
+ */
+public class Ejercicio01_07 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(4 * (1.0 - (1 / 3) + (1 / 5) -
+			(1 / 7) + (1 / 9) - (1 / 11)));
+	System.out.println(4 * (1.0 - (1 / 3) + (1 / 5) - (1 / 7)
+			+ (1 / 9) - (1 / 11) + (1 / 13)));
+    }
+    
+}
